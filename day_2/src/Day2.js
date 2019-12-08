15ebca1348085db21efff859ce051f6e2db151be
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-12-08 14:46:55 +0000

    Day 2 add tests passing

--- day_2/src/Day2.js
+++ day_2/src/Day2.js
@@ -2,24 +2,23 @@
 const intCode = (codes) => {
   let codesArray = codes.split(',').map(Number);
   console.log("codesArray", codesArray);
-  if (codesArray[0] === 99) {
-    return codesArray;
-  }
+
   else {
 
     for ( var i = 0; i < codesArray.length; i++ ) {
-      if (codesArray[i] === 1) {
+      if (codesArray[i] === 99) {
+        return codesArray;
+      }
+      else if (codesArray[i] === 1) {
         var firstPosition = codesArray[i + 1];
         var secondPosition = codesArray[i + 2];
         var resultPosition = codesArray[i + 3];
-        codesArray[resultPosition] = codesArray[firstPosition] + 
+        codesArray[resultPosition] = codesArray[firstPosition] +
                                      codesArray[secondPosition];
 
 
       }
     }
-
-
   }
   return codesArray ;
 };
--- day_2/src/test/test.js
+++ day_2/src/test/test.js
@@ -16,6 +16,10 @@ describe('Day2', function() {
       assert.deepEqual(day2.intCode("1,0,0,0,1,0,0,0,99"), [4,0,0,0,1,0,0,0,99]);
     });
 
+    it('calculate total on two blocks, stop on 99', function() {
+      assert.deepEqual(day2.intCode("1,0,0,0,99,0,0,0,99"), [2,0,0,0,99,0,0,0,99]);
+    });
+
 
   });
 });
