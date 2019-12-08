2bdb916d15e57e7c9e891fa5dd75c4d12f242127
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-12-08 14:58:14 +0000

    Day 2 multiply tests passing

--- day_2/src/Day2.js
+++ day_2/src/Day2.js
@@ -3,13 +3,11 @@ const intCode = (codes) => {
   let codesArray = codes.split(',').map(Number);
   console.log("codesArray", codesArray);
 
-  else {
 
-    for ( var i = 0; i < codesArray.length; i++ ) {
+    for ( var i = 0; i < codesArray.length; i += 4 ) {
       if (codesArray[i] === 99) {
         return codesArray;
-      }
-      else if (codesArray[i] === 1) {
+      } else if (codesArray[i] === 1) {
         var firstPosition = codesArray[i + 1];
         var secondPosition = codesArray[i + 2];
         var resultPosition = codesArray[i + 3];
@@ -17,9 +15,17 @@ const intCode = (codes) => {
                                      codesArray[secondPosition];
 
 
+      } else if (codesArray[i] === 2) {
+        var firstPosition = codesArray[i + 1];
+        var secondPosition = codesArray[i + 2];
+        var resultPosition = codesArray[i + 3];
+        codesArray[resultPosition] = codesArray[firstPosition] *
+                                     codesArray[secondPosition];
+
+
       }
     }
-  }
+
   return codesArray ;
 };
 
--- day_2/src/test/test.js
+++ day_2/src/test/test.js
@@ -4,7 +4,7 @@ var day2 = require('../Day2');
 
 describe('Day2', function() {
   describe('#IntCode()', function() {
-    it('code 99 stops the program and returns f', function() {
+    it('code 99 stops the program', function() {
       assert.deepEqual(day2.intCode("99"), [ 99 ]);
     });
 
@@ -18,6 +18,14 @@ describe('Day2', function() {
 
     it('calculate total on two blocks, stop on 99', function() {
       assert.deepEqual(day2.intCode("1,0,0,0,99,0,0,0,99"), [2,0,0,0,99,0,0,0,99]);
+    });
+
+    it('calculate total on 1 block, multiply', function() {
+      assert.deepEqual(day2.intCode("2,4,4,5,99,0"), [2,4,4,5,99,9801]);
+    });
+
+    it('calculate total on 2 block, multiply', function() {
+      assert.deepEqual(day2.intCode("1,1,1,4,99,5,6,0,99"), [30,1,1,4,2,5,6,0,99]);
     });
 
 
