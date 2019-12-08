ca86b524c7c6873f2ea9de5a94fb94a910a53b71
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-12-08 12:53:01 +0000

    add mocha test framework and first test passing

--- day_1/src/test/test.js
+++ day_1/src/test/test.js
@@ -7,5 +7,11 @@ describe('Day1', function() {
     it('should return 0 when passed zero', function() {
       assert.equal(day1.calculateFuelForFuel(0), 0);
     });
+
+    it('should return 2 on 1 level of recursion', function() {
+      assert.equal(day1.calculateFuelForFuel(14), 2);
+    });
+
+
   });
 });
