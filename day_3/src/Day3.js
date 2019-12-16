9954365b16131d66732befe1bf532eda5c6b5570
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-12-16 12:14:19 +0000

    first function complete

--- day_3/src/Day3.js
+++ day_3/src/Day3.js
@@ -1,23 +1,25 @@
 
-const generateCoordinates = (start, input) => {
+const generateEndCoordinates = (start, input) => {
 
- let direction = input[0].charAt(0);
- let distance = input[0].charAt(1);
-
- switch(direction) {
-   case "R":
-
-   return [[start[0] + distance,start[1]]];
-   break;
-   case "L":
-   return [[start[0] - distance,start[1]]];
-   break;
-   case "U":
-   return [[start[0],start[1] + distance]];
-   case "D":
-   return [[start[0],start[1] - distance]];
- }
+  let direction = input[0].charAt(0);
+  let distance = input[0].charAt(1);
+  coordinates = [];
+  switch(direction) {
+    case "R":
+    coordinates.push([start[0] + distance, start[1]]);
+    break;
+    case "L":
+    coordinates.push([start[0] - distance, start[1]]);
+    break;
+    case "U":
+    coordinates.push([start[0], start[1] + distance]);
+    break;
+    case "D":
+    coordinates.push([start[0], start[1] - distance]);
+    break;
+  }
+  return coordinates;
 
 };
 
-exports.generateCoordinates = generateCoordinates;
+exports.generateEndCoordinates = generateEndCoordinates;
--- day_3/src/test/test.js
+++ day_3/src/test/test.js
@@ -4,23 +4,24 @@ var day3 = require('../Day3');
 // test
 // '[R8]' => [[0,8]]
 describe('Day3', function() {
-  describe('#generateCoordinates()', function() {
+  describe('#generateEndCoordinates()', function() {
     it('1 input right', function() {
-      assert.deepEqual(day3.generateCoordinates([0,0],['R8']), [[8,0]]);
+      assert.deepEqual(day3.generateEndCoordinates([0,0],['R8']), [[8,0]]);
     });
     it('1 input left', function() {
-      assert.deepEqual(day3.generateCoordinates([0,0],['L8']), [[-8,0]]);
+      assert.deepEqual(day3.generateEndCoordinates([0,0],['L8']), [[-8,0]]);
     });
     it('1 input up', function() {
-      assert.deepEqual(day3.generateCoordinates([0,0],['U8']), [[0,8]]);
+      assert.deepEqual(day3.generateEndCoordinates([0,0],['U8']), [[0,8]]);
     });
 
-    it('multiple inputs same type', function() {
-      assert.deepEqual(day3.generateCoordinates([0,0],['U8','U2','U3']), [[0,-8]]);
+    it('1 input down', function() {
+      assert.deepEqual(day3.generateEndCoordinates([0,0],['D8']), [[0,-8]]);
     });
 
+  });
 
-
-
+  describe('#generateEndCoordinates()', function() {
+    
   });
 });
